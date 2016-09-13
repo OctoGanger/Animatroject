d2a83c4e8da5ded47a9b0e758c7d96a8b15494cb
OctoGanger <lannda.lathur@gmail.com>
2016-09-12 23:45:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/analuz/theotheranimation/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.analuz.theotheranimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+    public void movezergling(View view) {
+        ImageView zergling = (ImageView) findViewById(R.id.zergling);
+        Button button1 = (Button) findViewById(R.id.button1);
+
+    public void movezergling(View view) {
+        ImageView zergling = (ImageView) findViewById(R.id.zergling);
+        Button button2 = (Button) findViewById(R.id.button2);
